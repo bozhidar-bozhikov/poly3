ec7049f9e59e1cbed327c4dc1335d58dfca7ce7a
bozhidar-bozhikov <blbozhikov@gmail.com>
2025-10-27 01:37:38 +0100

    drawShape method
    Added a dirty drawShape method to draw a polygon on the display with the appropriate color. Added utility functions to display.h

--- Poly3/Core/Inc/display.h
+++ Poly3/Core/Inc/display.h
@@ -5,6 +5,8 @@
  *      Author: bojos
  */
 #include <stdint.h>
+#include <math.h>
+#include "st7789.h"
 #include "sequencer.h"
 
 #ifndef INC_DISPLAY_H_
@@ -12,12 +14,39 @@
 
 #define CIRCLE_CENTER_X 120
 #define CIRCLE_CENTER_Y 120
-#define CIRCLE_RADIUS 117
+#define CIRCLE_RADIUS 113
 
 typedef struct
 {
     int32_t x;
     int32_t y;
 } Point;
+
+typedef struct
+{
+    int32_t r;
+    int32_t phi;
+} Polar;
+
+void Display_Init(void);
+void Display_Main(void);
+void DrawShape(int32_t index);
+void DrawShapes(void);
+
+static inline Point polarToCartesian(int32_t r, float phi) {
+	Point p;
+	p.x = r * cos(phi * M_PI / 180);
+	p.y = r * sin(phi * M_PI / 180);
+	return p;
+}
+
+static inline Polar cartesianToPolar(int32_t x, int32_t y) {
+	Polar p;
+	p.r = sqrt(x*x + y*y);
+	p.phi = atan2(y, x);
+	return p;
+}
+
+uint16_t getColorForIndex(int32_t index);
 
 #endif /* INC_DISPLAY_H_ */
